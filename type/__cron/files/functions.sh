afc76a4dc0f9a6b0477f2985afa4f8da4bf30d89
Dennis Camera <dennis.camera@riiengineering.ch>
2025-01-31 22:29:10 +0100

    Add missing copyright headers

--- type/__cron/files/functions.sh
+++ type/__cron/files/functions.sh
@@ -1,3 +1,23 @@
+# NOTE: this file needs to be sourced.
+#
+# 2022 Dennis Camera (skonfig at dtnr.ch)
+#
+# This file is part of skonfig-base.
+#
+# skonfig-base is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# skonfig-base is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with skonfig-base. If not, see <http://www.gnu.org/licenses/>.
+#
+
 NL=$(printf '\n '); NL=${NL% }
 
 quote() { printf '%s\n' "$*" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/'/"; }
--- type/__key_value/files/remote_script.sh
+++ type/__key_value/files/remote_script.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+#
+# 2014 Daniel Heule (hda at sfs.biz)
+# 2018 Darko Poljak (darko.poljak at gmail.com)
+# 2020,2023 Dennis Camera (dennis.camera at riiengineering.ch)
+#
+# This file is part of skonfig-base.
+#
+# skonfig-base is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# skonfig-base is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with skonfig-base. If not, see <http://www.gnu.org/licenses/>.
+#
 
 key="$(cat "$__object/parameter/key" 2>/dev/null \
    || echo "$__object_id")"
